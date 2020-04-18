8e03a0a96f2945204ef0fef985a84bbdb8b2aac3
joel-klein <joel_klein1@yahoo.co.uk>
2020-04-18 09:43:24 +0100

    pages update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 
 const Contact = () => (
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
